9e5ade629df8c224962c5865df0f4adbc29b9b69
Winner Emeto <emetowinner@gmail.com>
2020-09-29 11:12:31 +0100

    Formatted code base using PEP8 standards

--- alphaorm/utilities/constants.py
+++ alphaorm/utilities/constants.py
@@ -6,17 +6,22 @@ UNEQUAL_BOUNDED_PARAMETER = 'Number of bounded parameters is not equal to variab
 DB_VARIABLE_ERROR = 'Values of can only be number, string or boolean'
 RECORD_NOT_STORED = 'This record is not stored yet'
 
+
 def SETUP_PARAMETER_MISSING(paremeter):
-	return f"The '{paremeter}' is required!"
+    return f"The '{paremeter}' is required!"
+
 
 def DATA_TYPE_ERROR(method):
-	return f"Parameter passed into method `{method}` must be of type `AlphaRecord`"
+    return f"Parameter passed into method `{method}` must be of type `AlphaRecord`"
+
 
 def DRIVER_NOT_SUPPORTED(driver):
-	return f"'{driver}' is not a supported database. Supported databases includes mysql"
+    return f"'{driver}' is not a supported database. Supported databases includes mysql"
+
 
 def SETUP_OPTION_MISSING(option):
-	return f"The '{option}' option is required for this database!"
+    return f"The '{option}' option is required for this database!"
+
 
 def VARIABLE_NOT_PRESENT(var):
-	return f"Variable '{var}' is not present in parameters"
+    return f"Variable '{var}' is not present in parameters"
